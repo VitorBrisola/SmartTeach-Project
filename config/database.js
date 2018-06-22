908756c45289850cd684b7216d81b3c1faa2669b
Vitor Brisola <vitoraugusto199@gmail.com>
2018-06-22 15:32:40 -0300

    Merge com a brach leo

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = 'mongodb://leoMurtha:041097ll@ds147890.mlab.com:47890/videas';
 }else{
-	module.exports = 'mongodb://127.0.0.1:27017/smartteach-dev';
+	module.exports = 'mongodb://mongo:27017/smartteach-dev';
 }
