b19a656201658286c6026801a421f64fe1ccfbc0
leoMurtha <aka.murtha@gmail.com>
2018-07-03 13:56:31 -0300

    a

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = 'mongodb://leoMurtha:041097ll@ds147890.mlab.com:47890/videas';
 }else{
-	module.exports = 'mongodb://mongo:27017/smartteach-dev';
+	module.exports = 'mongodb://127.0.0.1:27017/smartteach-dev';
 }
 
 /* DOCKER URI mongodb://mongo:27017/smartteach-dev 
