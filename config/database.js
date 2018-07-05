2151804ba72a8f1b12ded5920618bd029d1f9b34
leoMurtha <aka.murtha@gmail.com>
2018-07-04 22:17:40 -0300

    a

--- config/database.js
+++
@@ -1,9 +0,0 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = 'mongodb://admin:admin$1@ds127811.mlab.com:27811/smartteach';
-}else{
-	module.exports = 'mongodb://admin:admin$1@ds127811.mlab.com:27811/smartteach';
-}
-
-/* DOCKER URI mongodb://mongo:27017/smartteach-dev 
-   LOCALHOST URI mongodb://127.0.0.1:27017/smartteach-dev
-*/
