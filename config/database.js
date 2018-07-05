7811d856233a4dd7f673394a4adae3353576f758
leoMurtha <aka.murtha@gmail.com>
2018-07-04 22:10:34 -0300

    force merge

--- config/database.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://leoMurtha:041097ll@ds147890.mlab.com:47890/videas'}
-}else{
-    module.exports = {mongoURI: 'mongodb://127.0.0.1:27017/smartteach-dev'}
-}
--- helpers/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    ensureAuthenticated: function(req, res, next){
-        if(req.isAuthenticated()){
-            return next(); // Next middleware
-        }
-        req.flash('error_msg', 'Not Authorized, Please Login!');
-        res.redirect('/users/login');
-    }
-}
